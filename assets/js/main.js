f426b662325edc067bc2d894f267e6e21249cf60
arjun <arjun@localhost.localdomain>
2023-04-19 11:47:56 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const navMenu=document.getElementById('nav-menu'),
+navToggle=document.getElementById('nav-toggle'),
+navClose=document.getElementById('nav__close')
+
+if (navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if (navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
