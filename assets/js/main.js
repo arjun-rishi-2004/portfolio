53d95ab9431425daa280828086d41db459ae5f06
arjun <arjun@localhost.localdomain>
2023-04-19 14:05:26 +0530

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,10 @@ if (navClose){
 }
 
 
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu=document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n=>n.addEventListener('click',linkAction))
